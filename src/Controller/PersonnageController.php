c9c05e7eb6b36c20cd082f6d4283b302cdf2c122
klopfstein <dimitri.kft@gmail.com>
2020-05-13 14:43:44 +0200

    add controller PersonnageController and Init view twig

---
+++ src/Controller/PersonnageController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonnageController extends AbstractController
+{
+    /**
+     * @Route("/", name="personnage")
+     */
+    public function index()
+    {
+        return $this->render('personnage/index.html.twig');
+    }
+
+
+     /**
+     * @Route("/persos", name="persos")
+     */
+    public function persos()
+    {
+        return $this->render('personnage/persos.html.twig', [
+            "pseudo" => "toto",
+            "age" => 25,
+            "carac" => [
+                "force" => 3,
+                "agi"   => 2,
+                "intel" => 3
+            ]
+        ]);
+    }
+}
